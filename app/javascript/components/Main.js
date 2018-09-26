07b4467b81f1210b9c36e8eaebaa50c16ca66de7
Amanda Himmerich <amanda.himmerich@gmail.com>
2018-09-26 18:24:53 -0500

    New Components

---
+++ app/javascript/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApartmentListings from '../components/ApartmentListings';
+
+const Main = (props) => {
+    return(
+      <div>
+        <h1>Fruits are great!</h1>
+        <ApartmentListings />
+      </div>
+    )
+}
+
+export default Main;
